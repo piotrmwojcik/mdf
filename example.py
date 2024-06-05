72784e4f48ef1d252a2de81cc0506bc583a76cfd
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-05 17:03:25 +0200

    change sample

--- example.py
+++ example.py
@@ -62,7 +62,7 @@ if cuda_available:
 imgrb = Variable( imgr, requires_grad = False)
 imgdb = Variable( imgd, requires_grad = True)
 
-optimizer = optim.Adam([imgdb], lr=0.1)
+optimizer = optim.Adam([imgdb], lr=0.2)
 
 # Initialise the loss
 criterion = MDFLoss(path_disc, cuda_available=cuda_available)
