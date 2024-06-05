80c7a2d88a334c8fb212c73be94fa7b2c4837027
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-05 18:35:53 +0200

    change sample

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ from mdfloss import MDFLoss
 
 # Set parameters
 cuda_available = True
-epochs = 300
+epochs = 200
 application = 'JPEG'
 image_path = './misc/mp_scene_0000_002.png'
 pickle_file_path = './misc/scene_0000.pkl'
