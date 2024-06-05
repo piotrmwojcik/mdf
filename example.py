7a68582027faa38bbd477425b4a0dc4b58847b45
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-05 16:45:30 +0200

    change sample

--- example.py
+++ example.py
@@ -50,9 +50,6 @@ imgr = imgr.unsqueeze(0).type(torch.FloatTensor)
 loaded_tensor = load_tensor_from_pickle(pickle_file_path)
 loaded_tensor = loaded_tensor.reshape(6, 3, 128, 128)
 imgd = loaded_tensor[2, :, :, :].unsqueeze(0)
-print('!!')
-print(imgr.shape)
-print(imgd.shape)
 
 # Save the original state
 imgdo = imgd.detach().clone()
@@ -82,21 +79,23 @@ for ii in range(0,epochs):
     
 
 # Convert images to numpy
-imgrnp = imgr.cpu().squeeze(0).permute(1,2,0).data.numpy()
-imgdnp = imgd.cpu().squeeze(0).permute(1,2,0).data.numpy()
-imgdonp = imgdo.cpu().squeeze(0).permute(1,2,0).data.numpy()
+imgrnp = imgr.cpu().squeeze(0).permute(1, 2, 0).clamp(0, 1).data.numpy()
+imgdnp = imgd.cpu().squeeze(0).permute(1, 2, 0).clamp(0, 1).data.numpy()
+imgdonp = imgdo.cpu().squeeze(0).permute(1, 2, 0).clamp(0, 1).data.numpy()
 
 
-# Plot optimization results
-fig, axs = plt.subplots(1, 3,figsize=(45,15))
+# List of images and their titles
+images = [imgdonp, imgdnp, imgrnp]
+titles = ['Noisy image', 'Recovered image', 'Reference image']
 
-
-axs[0].imshow(imgdonp)
-axs[0].set_title('Noisy image',fontsize=48)
-axs[1].imshow(imgdnp)
-axs[1].set_title('Recovered image',fontsize=48)
-axs[2].imshow(imgrnp)
-axs[2].set_title('Reference image',fontsize=48)
+# Iterate over the images and titles, and save each image with its title
+for i, (image, title) in enumerate(zip(images, titles)):
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    ax.set_title(title, fontsize=48)
+    ax.axis('off')  # Hide the axes
+    plt.savefig(f'image_{i}.png', bbox_inches='tight')  # Save the figure
+    plt.close(fig)  # Close the figure to free memory
 
 # Remove the ticks from the axis
 for ax in axs:
