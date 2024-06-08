163388e04776b53da61e18dee8404972e7bded87
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-08 13:48:18 +0200

    change sample

--- example2.py
+++ example2.py
@@ -13,7 +13,7 @@ from mdfloss import MDFLoss
 
 # Set parameters
 cuda_available = True
-epochs = 250
+epochs = 400
 application = 'JPEG'
 image_path = './misc/mp_scene_0000_002.png'
 code_file_path = './misc/code.pkl'
