8f2f1486757d9c6bcdefc559e9f52dc30dc32e58
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-10 14:33:20 +0200

    change sample

--- example2.py
+++ example2.py
@@ -57,6 +57,9 @@ imgr = loaded_tensor
 wpr = (imgr == 1.0).all(dim=1).sum(dim=(1, 2)) / (128.0 * 128.0)
 wpr = nn.Softmax(dim=0)(wpr * wpr)
 
+print('!!!')
+print(wpr)
+
 # Save the original state
 imgdo = imgd.detach().clone()
 
