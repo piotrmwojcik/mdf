604c8d53da9688e29dd9bc93d79ae7ab8f644661
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-12 14:00:30 +0200

    change sample

--- example2.py
+++ example2.py
@@ -56,7 +56,7 @@ imgr = loaded_tensor
 
 wpr = (imgr == 1.0).all(dim=1).sum(dim=(1, 2)) / (128.0 * 128.0)
 #wpr = nn.Softmax(dim=0)(wpr * wpr * wpr)
-wpr = torch.tensor([3.0, 3.0, 1.0, 1.0]).cuda()
+wpr = torch.tensor([10.0, 10.0, 1.0, 1.0]).cuda()
 
 # Save the original state
 imgdo = imgd.detach().clone()
